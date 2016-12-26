44fa3594df5baeb40792330cceca7448d92a2ad8
Svetlana Staneva <smstaneva@gmail.com>
2016-12-26 18:43:56 +0000

    Dynamically Load Menu Categories View, Part 2

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -101,6 +101,8 @@ global.$dc = dc;
 		var html = categoryHtml;
 		var name = "" + categories[i].name;
 		var short_name = categories[i].short_name;
+		html =
+      insertProperty(html, "name", name);
 		html = insertProperty(html, "short_name", short_name);
 		finalHtml += html;
 	}
