3d4c094dd0f30f9ddb9147009a05d9758d0dbffe
Svetlana Staneva <smstaneva@gmail.com>
2016-12-26 16:06:44 +0000

    Dynamically Load Home View Content

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -1,8 +1,28 @@
-$(function(){
-	$("#navbarToggle").blur(function(event){
-		var screenWidth = window.innerWidth;
-		if(screenWidth < 768){
-			$("#collapsable-nav").collapse('hide');	
-		}	
+
+(function (global){
+	var dc = {};
+	var homeHtml = "snippets/home-snippet.html";
+	var insertHtml = function (selector, html){
+		var targetElem = document.querySelector(selector);
+		targetElem.innerHTML = html;
+	};
+	
+	var showLoading = function(selector){
+		var html = "<div class='text-center'>";
+		html += "<img src='images/ajax-loader.gif'></div>";
+		insertHtml(selector, html);
+	};
+	
+	document.addEventListener("DOMContentLoaded", function (event){
+		showLoading("#main-content");
+		$ajaxUtils.sendGetRequest(
+		homeHtml,
+		function (responseText){
+			document.querySelector("#main-content")
+				.innerHTML = responseText;	
+		},
+		false);		
 	});
-});
+global.$dc = dc;
+})(window);
+
